af3397a5d7ef9bbbb399469500b865111dd8ffa2
M-Jhay <elugwujecinta@gmail.com>
2022-04-18 23:16:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var container = document.querySelector('.container');
+var outputScreen = document.querySelector('#outputScreen');
+
+function display(num){
+    outputScreen.value +=num
+}
+
+function calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+
+    catch(err){
+        alert("invalid")
+    }
+}
+
+function clear(){
+    outputScreen.value = "";
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1)
+}
